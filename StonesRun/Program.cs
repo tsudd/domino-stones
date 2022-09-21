a3113dfe6bef1a0cb9a590d92e9d542c257a58e5
Aliaksandr Khrystsenka <alex.kr1stehko@gmail.com>
2022-09-21 21:27:27 +0300

    add: console starter with input

--- StonesRun/Program.cs
+++ StonesRun/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Input random sequence of domino stones in the specified format (\'[1|2] [2|5] ...\')");
+var stonesSequence = Console.ReadLine() ?? "";
+
+try
+{
+    var dominoCollection = new DominoCollection(stonesSequence);
+
+    var answer = dominoCollection.FindCircle();
+    Console.WriteLine(answer);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Wrong sequence format: {ex.Message}");
+}
+catch (AggregateException ex)
+{
+    Console.WriteLine($"Couldn't find possible circle in the sequence: {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Something went wrong: {ex.Message}");
+}
